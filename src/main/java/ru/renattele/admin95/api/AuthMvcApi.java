d89bbf28376a712e667d0984e3c7d2f5b466c98a
renattele <renattele@yandex.com>
2025-05-17 01:37:45 +0300

    Refactor AuthMvcApi and configuration; remove CrossOrigin annotation and update Caddyfile for cleaner setup

--- src/main/java/ru/renattele/admin95/api/AuthMvcApi.java
+++ src/main/java/ru/renattele/admin95/api/AuthMvcApi.java
@@ -2,14 +2,12 @@ package ru.renattele.admin95.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/auth")
 public interface AuthMvcApi {
     @GetMapping("/check/terminal")
-    @CrossOrigin
     @PreAuthorize("hasRole('ACCESS_TERMINAL')")
     ResponseEntity<String> check();
 }
